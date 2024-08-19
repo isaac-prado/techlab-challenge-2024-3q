5b5e7c25ca8ae6b19e3b7f2917809187f90e7ed4
Isaac <isaacpalmeida@unifei.edu.br>
2024-08-19 20:52:51 -0300

    adicinando hashing na migration

--- backend/src/migrations/1719291159178-RootUser.ts
+++ backend/src/migrations/1719291159178-RootUser.ts
@@ -1,13 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 import { User } from "../entities/User.js";
+import { hash } from "bcrypt";
 
 export class RootUser1719291159178 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
+        const hashedPassword = await hash('root', 10);
+
         await queryRunner.manager.insert(User, {
             email: 'root@root.com',
             username: 'root',
-            password: 'root',
+            password: hashedPassword,
             profile: 'sudo'
         })
     }
